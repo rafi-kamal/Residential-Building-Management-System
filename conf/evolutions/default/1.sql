1f1171d0fb903fd4068e1294c45bc2b21ab2a663
Nawfil Afif Nibir <na.nibir@gmail.com>
2014-03-22 22:47:02 +0600

    Added Thread

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,14 +5,29 @@
 
 create table message (
   internal_id               bigint not null,
+  THREAD_ID                 bigint not null,
   time_stamp                time,
   body                      varchar(255),
   is_read                   boolean,
   constraint pk_message primary key (internal_id))
 ;
 
+create table thread (
+  internal_id               bigint not null,
+  category                  varchar(255),
+  date                      date,
+  subject                   varchar(255),
+  sender_id                 bigint,
+  receiver_id               bigint,
+  constraint pk_thread primary key (internal_id))
+;
+
 create sequence message_seq;
 
+create sequence thread_seq;
+
+alter table message add constraint fk_message_thread_1 foreign key (THREAD_ID) references thread (internal_id) on delete restrict on update restrict;
+create index ix_message_thread_1 on message (THREAD_ID);
 
 
 
@@ -22,7 +37,11 @@ SET REFERENTIAL_INTEGRITY FALSE;
 
 drop table if exists message;
 
+drop table if exists thread;
+
 SET REFERENTIAL_INTEGRITY TRUE;
 
 drop sequence if exists message_seq;
+
+drop sequence if exists thread_seq;
 
