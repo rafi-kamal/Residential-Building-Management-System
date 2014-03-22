1e0a851e61bb5a9b72ddef578189e3afdf74b701
Mukut <jamshed.mukut@gmail.com>
2014-03-22 23:05:09 +0600

    Bill

---
+++ app/models/Bill.java
@@ -0,0 +1,38 @@
+package models;
+
+import org.joda.time.LocalTime;
+
+import play.data.*;
+import play.data.validation.Constraints.*;
+import javax.validation.Valid;
+
+import javax.persistence.*;
+
+@Entity
+public class Bill extends play.db.ebean.Model {
+	@Id
+	@GeneratedValue
+	public Long id;
+	
+	@Required
+	public Long apartmentBuildingId;
+	
+	@Required
+	public Long apartmentNo;
+	
+	public String description;
+	
+	@Required
+	public LocalTime dateOfIssuing;
+	
+	@Required
+	public LocalTime deadline;
+	
+	@Required
+	public String status;
+	
+	@Required
+	public Double amount;
+	
+	
+}
--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,6 +3,18 @@
 
 # --- !Ups
 
+create table bill (
+  id                        bigint not null,
+  apartment_building_id     bigint,
+  apartment_no              bigint,
+  description               varchar(255),
+  date_of_issuing           time,
+  deadline                  time,
+  status                    varchar(255),
+  amount                    double,
+  constraint pk_bill primary key (id))
+;
+
 create table message (
   internal_id               bigint not null,
   time_stamp                time,
@@ -10,6 +22,8 @@ create table message (
   is_read                   boolean,
   constraint pk_message primary key (internal_id))
 ;
+
+create sequence bill_seq;
 
 create sequence message_seq;
 
@@ -20,9 +34,13 @@ create sequence message_seq;
 
 SET REFERENTIAL_INTEGRITY FALSE;
 
+drop table if exists bill;
+
 drop table if exists message;
 
 SET REFERENTIAL_INTEGRITY TRUE;
+
+drop sequence if exists bill_seq;
 
 drop sequence if exists message_seq;
 
