8187d73c6daa226ce70fb3b9cb904b87728e1a5e
Rafi Kamal <rafikamal93@gmail.com>
2014-03-24 18:23:17 +0600

    subpackaging

--- app/controllers/account/ProfileController.scala
+++ app/controllers/account/ProfileController.scala
@@ -3,7 +3,7 @@ package controllers.account
 import play.api.mvc.{Action, Result}
 import play.api.mvc.Controller
 
-object Profile extends Controller {
+object ProfileController extends Controller {
 	def index = Action {
 	  Ok(views.html.account.profile.render())
 	}
--- app/controllers/account/UserAccountController.java
+++
@@ -1,9 +0,0 @@
-package controllers.account;
-
-import models.UserAccount;
-import play.data.Form;
-import play.mvc.Controller;
-
-public class UserAccountController extends Controller {
-	static Form<UserAccount> userForm = Form.form(UserAccount.class);
-}
