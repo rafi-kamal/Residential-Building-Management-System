08795d4fc7afe137ce4c34abdbc5980b5172b242
Rafi Kamal <rafikamal93@gmail.com>
2014-04-01 03:00:43 +0600

    fetching apartment buildings

---
+++ app/controllers/account/SelectApartmentController.java
@@ -0,0 +1,32 @@
+package controllers.account;
+
+import java.util.List;
+
+import models.ApartmentBuilding;
+import models.RealEstateCompany;
+import play.libs.Json;
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class SelectApartmentController extends Controller {
+    
+    public static Result index() {
+    	List<RealEstateCompany> companies = RealEstateCompany.find.all();
+    	return ok(views.html.account.selectApartment.render(companies));
+    }
+    
+    public static Result getApartmentBuildings(Long realEstateId) {
+    	List<ApartmentBuilding> apartmentBuildings = ApartmentBuilding.find
+    			.where()
+    			.eq("real_estate_company_id", realEstateId);
+    	
+    	return ok(Json.toJson(apartmentBuildings));
+    }
+    
+    public static Result select() {
+    	
+    	return ok(views.html.account.showAllProfiles.render(apartments));
+    }
+}
