defea50de1775cf7d422b599a6adde3b6fa0b200
Nawfil Afif Nibir <na.nibir@gmail.com>
2014-04-01 05:43:23 +0600

    Thread works (again!)

--- app/controllers/account/ProfileController.java
+++ app/controllers/account/ProfileController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import models.Apartment;
 import models.ApartmentBuilding;
 import models.UserAccount;
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -27,8 +28,9 @@ public class ProfileController extends Controller {
     public static Result showAllProfiles()
     {
     	String userId = session().get("userId");
-    	UserAccount userAccount = UserAccount.find.where().eq("userId", userId).findUnique();
-    	ApartmentBuilding building = ApartmentBuilding.find.where().eq("id", userAccount.apartment.apartmentBuilding.id).findUnique();
+    	UserAccount userAccount = UserAccount.find.byId(Long.parseLong(userId));
+    	Logger.debug(String.valueOf(userAccount.apartment.apartmentBuilding.id));
+    	ApartmentBuilding building = ApartmentBuilding.find.byId(userAccount.apartment.apartmentBuilding.id);
     	List<Apartment> apartments = Apartment.find.where().eq("apartment_building_id", building.id).findList();
     	return ok(views.html.account.showAllProfiles.render(apartments));
     }
